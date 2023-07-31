36c48de5dc988c91cdaf272581175cb73d36cbd5
_haoqi <1148648445@qq.com>
2023-07-31 17:04:29 +0800

    3

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,8 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'b>(x: &'b str, y: &'b str) -> &'b str {
     if x.len() > y.len() {
         x
     } else {
@@ -20,7 +19,7 @@ fn longest(x: &str, y: &str) -> &str {
 
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = "xyz";
+    let string2: &'static str = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is '{}'", result);
